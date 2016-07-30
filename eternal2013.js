486a14d8c18680be5f76ff30590015868537c12d
mogesystem <moge32@gmail.com>
2016-07-31 01:00:37 +0900

    Initial commit

---
+++ eternal2013.js
@@ -0,0 +1,116 @@
+﻿
+// エターナル2013年
+// eternal2013.js
+
+new function() {
+	// ブラウザが対応しているオーディオ形式を選択
+	var audio_ext = (function() {
+		var audio = new Audio();
+		if (audio.canPlayType("audio/ogg") != "") { return "ogg"; }
+		return "mp3";
+	})();
+
+	var play_gong = function() {
+		var audio = new Audio("gong." + audio_ext);
+		audio.play();
+	};
+
+	// 普段クリックしたときの関数
+	var season_opacity = 1.0;
+	var season_delete = function() {
+		season_opacity = 1.0 - season_opacity;
+		$(".season").animate({opacity: season_opacity}, 500);
+	};
+
+	// 日付表示の更新
+	var refresh_time = function() {
+		var now = new Date();
+		var orig = new Date(2013, 12-1, 1);
+		var lapse_ms = now.getTime() - orig.getTime();
+		var lapse = Math.floor(lapse_ms/1000/60/60/24) + 1;
+		
+		var ymd = "2013/12/" + lapse;
+		var hms =
+			("0" + now.getHours()).slice(-2) + ":" +
+			("0" + now.getMinutes()).slice(-2) + ":" +
+			("0" + now.getSeconds()).slice(-2);
+		
+		if ($("#dt").text() != ymd) {
+			// 日付が変わった
+			$("#dt").html(ymd);
+			$("#twbtn").html(
+				"<a href=\"https://twitter.com/share\" " +
+				"class=\"twitter-share-button\"{count} " +
+				"data-url=\"https://mg32.github.io/eternal2013/\" " +
+				"data-text=\"エターナル2013年\n本日は 2013年12月" + lapse +
+				"日 です。\n\"data-lang=\"ja\">ツイート</a>\n");
+			if (window.twttr && window.twttr.widgets && window.twttr.widgets.load) {
+				window.twttr.widgets.load();
+			}
+			refresh_image();
+			refresh_snow();
+		}
+		
+		$("#tm").html(hms);
+	};
+
+	var refresh_image = function() {
+		var now = new Date();
+		var m = now.getMonth() + 1;
+		var d = now.getDate();
+		
+		var filename = "";
+		var clickfunc = null;
+		switch (m) {
+		case 7:
+		case 8:
+			// 7～8月は夏仕様
+			filename = "image/natsu" + Math.floor(Math.random()*5) + ".png";
+			clickfunc = season_delete;
+			break;
+			
+		case 12:
+			if (d >= 30) {
+				// 12/30, 12/31は大晦日仕様
+				filename = "image/gong.png";
+				clickfunc = play_gong;
+			} else {
+				// 12月はクリスマス仕様
+				filename = "image/xmas" + Math.round(Math.random()) + ".png";
+				clickfunc = season_delete;
+			}
+			break;
+		}
+		
+		// 画像ファイルを設定
+		if (filename != "") {
+			$(".seasonimg").each(function(i, elem) {
+				$(elem).attr("src", filename);
+			});
+		}
+		
+		// 画像クリック時のイベントを設定
+		if (clickfunc != null) {
+			$(".seasontop").unbind().click(clickfunc);
+		}
+	};
+
+	var refresh_snow = function() {
+		var now = new Date();
+		var m = now.getMonth() + 1;
+		
+		switch (m) {
+		case 12:
+		case 1:
+		case 2:
+			MgSnow.start();
+			break;
+		}
+	};
+
+	$(function() {
+		refresh_time(); setInterval(refresh_time, 1000);
+		refresh_image();
+		refresh_snow();
+	});
+};
---
+++ mgsnow.js
@@ -0,0 +1,78 @@
+﻿
+// エターナル2013年
+// snow.js
+
+var MgSnow = {};
+
+new function() {
+	var _count = 100;
+	var _pobj = [];
+	var _pd = [{}];
+	
+	MgSnow._width = 0;
+	MgSnow._height = 0;
+	
+	// 雪を降らせはじめる
+	MgSnow.start = function() {
+		onresize(); $(window).resize = onresize;
+		
+		for (var i = 0; i < _count; i++) {
+			create(i);
+		}
+		move(); setInterval(move, 40);
+	};
+	
+	// イベント: リサイズ
+	onresize = function() {
+		MgSnow._width = $(window).width();
+		MgSnow._height = $(window).height();
+	};
+
+	// 雪パーティクル作成
+	var create = function(id) {
+		var sz = Math.floor(Math.random() * 10 + 1);
+		var d = Math.floor(Math.random() * 8 + 1);
+		var r = Math.random() * Math.PI / 18;
+		
+		var p = $("<div>").attr("class", "particle").css({
+			"z-index": d, width: sz + "px", height: sz + "px"
+		});
+		
+		_pobj[id] = p;
+		_pd[id] = {px: 0, py: 0, size: sz, dist: d, rad: r, phase: 0};
+		reset(id);
+		
+		$("#snow").append(p);
+	};
+
+	// 雪: 移動
+	var move = function() {
+		for (var i = 0; i < _count; i++) {
+			fall(i);
+			if (_pd[i].py > MgSnow._height) {
+				reset(i);
+			}
+		}
+	};
+
+	// 雪: 位置初期化
+	var reset = function(id) {
+		_pd[id].px = Math.floor(Math.random() * (MgSnow._width + 40) - 20);
+		_pd[id].py = -Math.floor(Math.random() * MgSnow._height);
+		
+		_pobj[id].css({
+			left: _pd[id].px + "px", top: _pd[id].py + "px"
+		});
+	};
+
+	// 雪: 下へ
+	var fall = function(id) {
+		var x = _pd[id].px + _pd[id].dist * Math.cos(_pd[id].phase);
+		_pd[id].py += _pd[id].size;
+		_pd[id].phase += _pd[id].rad;
+		
+		_pobj[id].css({
+			left: x + "px", top: _pd[id].py + "px"
+		});
+	};
+};
